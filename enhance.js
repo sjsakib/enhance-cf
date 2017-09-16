3de333a8ae6835de724396da4af2f6a7fabb4856
S.j. Sakib <sjsakib.bd@gmail.com>
2017-09-16 14:57:45 +0600

    added copy

--- enhance.js
+++ enhance.js
@@ -1,37 +1,78 @@
 var url = window.location.pathname;
 
-var sidebar = document.getElementById('sidebar');
-var subsDiv = document.createElement('div');
-subsDiv.className = 'roundbox sidebox sidebar-menu';
-subsDiv.style = '';
+addCopyLink();
 
-var titleDiv = document.createElement('div');
-titleDiv.className = "caption titled";
-titleDiv.appendChild(document.createTextNode('→ Solutions'))
-subsDiv.appendChild(titleDiv);
+addAllSubs(url);
 
-var targetURL = getTargetUrl(url);
-var link = document.createElement('a')
-link.href = targetURL;
-link.appendChild(document.createTextNode('All Submissions'));
 
-var ul = document.createElement('ul');
-var li = document.createElement('li');
-var span = document.createElement('span');
-span.appendChild(link);
-li.appendChild(span);
-ul.appendChild(li);
-subsDiv.appendChild(ul);
+function addCopyLink() {
+  var sampleTests = document.getElementsByClassName('sample-test')[0];
+  sampleTests.childNodes.forEach( (el) => {
+    var link = document.createElement('a');
+    link.text = '(copy)';
+    link.href = '#';
+    link.style="font-size:.8em;font-weight:lighter;float:right"
+    el.childNodes[0].append(link);
+    link.addEventListener('click', copyOnClick );
+  });
 
-sidebar.appendChild(subsDiv);
+}
+
+
+function addAllSubs(url) {
+    var sidebar = document.getElementById('sidebar');
+    var subsDiv = document.createElement('div');
+    subsDiv.className = 'roundbox sidebox sidebar-menu';
+    subsDiv.style = '';
+
+    var titleDiv = document.createElement('div');
+    titleDiv.className = "caption titled";
+    titleDiv.appendChild(document.createTextNode('→ Solutions'))
+    subsDiv.appendChild(titleDiv);
+
+    var targetURL = getTargetUrl(url);
+    var link = document.createElement('a')
+    link.href = targetURL;
+    link.appendChild(document.createTextNode('All Submissions'));
+
+    var ul = document.createElement('ul');
+    var li = document.createElement('li');
+    var span = document.createElement('span');
+    span.appendChild(link);
+    li.appendChild(span);
+    ul.appendChild(li);
+    subsDiv.appendChild(ul);
+
+    sidebar.appendChild(subsDiv);
+}
 
 
 function getTargetUrl(url) {
-  var args = url.split('/');
-  if (args[1] == 'problemset' && args[2] == 'problem') {
-    var targetURL = "http://codeforces.com/problemset/status/" + args[3] + "/problem/" + args[4];
-  } else {
-    var targetURL = "http://codeforces.com/contest/" + args[2] + "/status/" + args[4];
-  }
-  return targetURL;
+    var args = url.split('/');
+    if (args[1] == 'problemset' && args[2] == 'problem') {
+        var targetURL = "http://codeforces.com/problemset/status/" + args[3] + "/problem/" + args[4];
+    } else {
+        var targetURL = "http://codeforces.com/contest/" + args[2] + "/status/" + args[4];
+    }
+    return targetURL;
+}
+
+function copyOnClick(e) {
+  e.preventDefault();
+  var textCon = e.target.parentElement.nextElementSibling;
+
+  var inp = document.createElement('textarea');
+  document.querySelector('body').append(inp);
+  console.log(textCon.innerText);
+  inp.value = textCon.innerText;
+  inp.select();
+  document.execCommand('Copy');
+  inp.remove();
+  e.target.text = '(copied)';
+
+  document.getElementsByClassName('sample-test')[0].childNodes.forEach((el) => {
+    if(el.childNodes[0].childNodes[1] != e.target) {
+      el.childNodes[0].childNodes[1].text = '(copy)'
+    }
+  });
 }
