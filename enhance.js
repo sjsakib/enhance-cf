cb3f2abadb3b6b42d826fedf9ec32e68086b916c
S.j. Sakib <sjsakib.bd@gmail.com>
2017-09-16 12:15:11 +0600

    removed innerHTML

--- enhance.js
+++ enhance.js
@@ -1,20 +1,30 @@
-var sidebar = document.getElementById('sidebar');
-var subs = document.createElement('div');
-
-subs.className = 'roundbox sidebox sidebar-menu';
-subs.style = ''
-
 var url = window.location.pathname;
+
+var sidebar = document.getElementById('sidebar');
+var subsDiv = document.createElement('div');
+subsDiv.className = 'roundbox sidebox sidebar-menu';
+subsDiv.style = '';
+
+var titleDiv = document.createElement('div');
+titleDiv.className = "caption titled";
+titleDiv.appendChild(document.createTextNode('→ Solutions'))
+subsDiv.appendChild(titleDiv);
+
 var targetURL = getTargetUrl(url);
+var link = document.createElement('a')
+link.href = targetURL;
+link.appendChild(document.createTextNode('All Submissions'));
 
-var toAdd = "";
-toAdd += "<div class='roundbox-lt'>   </div>";
-toAdd += "<div class='roundbox-rt'>   </div>";
-toAdd += "<div class='caption titled'> → Solutions </div>";
-toAdd += "<ul> <li> <span> <a href='"+targetURL+"'>See All Submissions</a> </span> </li> </ul>";
+var ul = document.createElement('ul');
+var li = document.createElement('li');
+var span = document.createElement('span');
+span.appendChild(link);
+li.appendChild(span);
+ul.appendChild(li);
+subsDiv.appendChild(ul);
 
-subs.innerHTML += toAdd;
-sidebar.appendChild(subs);
+sidebar.appendChild(subsDiv);
+
 
 function getTargetUrl(url) {
   var args = url.split('/');
